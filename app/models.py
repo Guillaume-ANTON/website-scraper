1b9e358a8874bf6f22d111b98b8666dbbea23635
Guillaume Anton <guillaumeanton@MacBook-Pro-de-Guillaume.local>
2025-04-21 10:50:25 +0200

    feat(db): setup SQLAlchemy engine and article model

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Article(Base):
+    __tablename__ = "articles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
